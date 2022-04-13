bf054b197f048bc5c8770b3e3e3253c8d3027fbe
JackBecknell <52moosefit@gmail.com>
2022-04-13 14:31:05 -0400

    Created new app replies and created a new model 'replies'

---
+++ backend/drf_jwt_backend/replies/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from comments.models import Comment
+
+
+class Replies(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
